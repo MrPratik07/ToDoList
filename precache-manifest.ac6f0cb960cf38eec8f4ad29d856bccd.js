fbc45c0c03314e45ba116e977cb4dc4193b9964b
pratik <pratikgaikwad551@gmail.com>
2020-08-03 00:16:32 +0530

    Updates

---
+++ precache-manifest.ac6f0cb960cf38eec8f4ad29d856bccd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fc785915702d96a8f447e1828ec1f7",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "d9bfd34a6bd00d4aeb60",
+    "url": "/ToDoList/static/css/main.2321276e.chunk.css"
+  },
+  {
+    "revision": "92a48736877a6c5cbb08",
+    "url": "/ToDoList/static/js/2.b6e4df10.chunk.js"
+  },
+  {
+    "revision": "d9bfd34a6bd00d4aeb60",
+    "url": "/ToDoList/static/js/main.354df1aa.chunk.js"
+  },
+  {
+    "revision": "27c31e27ca80494135a3",
+    "url": "/ToDoList/static/js/runtime~main.a577f1c4.js"
+  }
+]);
